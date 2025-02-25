c52d2b36475e5755f14cea3f9b4d2d16617696d5
María del Carmen <jacqueelopez@gmail.com>
2025-02-25 16:13:48 +0100

    gitignore

---
+++ BD/bd.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE libreria;
+use libreria;
+
+create table libros(
+Idlibro int auto_increment primary key,
+titulo varchar(50),
+autor varchar(50),
+genero varchar(45)
+);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 //imports
 const express = require('express');
 const cors = require('cors');
+const mysql = require ("mysql2/promise");
 
 //crear el servidor
 const server = express();
+//DOTENV
+require ("dotenv").config();
 
 // configurar el servidor
 server.use(cors());
 server.use(express.json());
+
+
+//conexion a bd
+
+async function connectionbd() {
+  const connection = await mysql.createConnection({
+    host:"",
+    user:,
+    password,
+    database:
+  });
+}
 
 const PORT = 5005;
 server.listen(PORT, () => {
